596a2fb208405fcf76e5752bdb21e3cf08355f1a
Mark <markisme6383@gmail.com>
2026-03-03 13:30:36 +0300

    Добавил mutex в репозиторий, чтобы не было гонки данных при работе хэндлеров

--- repository/Warehouse.go
+++ repository/Warehouse.go
@@ -3,10 +3,12 @@ package repository
 import (
 	"Warehouse/errors"
 	"Warehouse/models"
+	"sync"
 )
 
 type Warehouse struct {
 	items map[string]models.Item
+	mtx   sync.RWMutex
 }
 
 func NewWarehouse() Warehouse {
@@ -16,6 +18,8 @@ func NewWarehouse() Warehouse {
 }
 
 func (w *Warehouse) AddItem(item models.Item) error {
+	w.mtx.Lock()
+	defer w.mtx.Unlock()
 	_, ok := w.items[item.Id]
 	if ok {
 		return errors.ErrItemAlreadyExist
@@ -27,6 +31,8 @@ func (w *Warehouse) AddItem(item models.Item) error {
 }
 
 func (w *Warehouse) DeleteItem(itemId string) error {
+	w.mtx.Lock()
+	defer w.mtx.Unlock()
 	_, ok := w.items[itemId]
 	if !ok {
 		return errors.ErrItemNotFound
@@ -38,6 +44,8 @@ func (w *Warehouse) DeleteItem(itemId string) error {
 }
 
 func (w *Warehouse) GetItem(itemId string) (models.Item, error) {
+	w.mtx.RLock()
+	defer w.mtx.RUnlock()
 	item, ok := w.items[itemId]
 	if !ok {
 		return models.Item{}, errors.ErrItemNotFound
@@ -47,10 +55,14 @@ func (w *Warehouse) GetItem(itemId string) (models.Item, error) {
 }
 
 func (w *Warehouse) GetAllTItems() map[string]models.Item {
+	w.mtx.RLock()
+	defer w.mtx.RUnlock()
 	return w.items
 }
 
 func (w *Warehouse) GetItemLighterThan(weight float64) map[string]models.Item {
+	w.mtx.RLock()
+	defer w.mtx.RUnlock()
 	filteredItems := make(map[string]models.Item)
 
 	for _, item := range w.items {
@@ -63,6 +75,8 @@ func (w *Warehouse) GetItemLighterThan(weight float64) map[string]models.Item {
 }
 
 func (w *Warehouse) ChangeItemTitle(itemId string, title string) (models.Item, error) {
+	w.mtx.RLock()
+	defer w.mtx.RUnlock()
 	item, ok := w.items[itemId]
 	if !ok {
 		return models.Item{}, errors.ErrItemNotFound
